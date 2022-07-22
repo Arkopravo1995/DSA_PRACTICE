b5b5c2f2446953d2047fa563baf307ca3835e8dd
Arkopravo Bhuin <99447101+Arkopravo1995@users.noreply.github.com>
2022-07-22 11:36:48 +0530

    Add files via upload

---
+++ ReverseArray.java
@@ -0,0 +1,14 @@
+package array;
+
+public class ReverseArray {
+	
+
+	public static void main(String[] args) {
+		int arr[] = {2,3,4,6,8};
+		for(int j = arr.length-1 ; j >=0 ; j-- ) {
+			System.out.print(arr[j]);
+		}
+
+	}
+
+}
