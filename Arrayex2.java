9570223fde9c4578fcf55173de2a126356d20b3d
Arkopravo Bhuin <99447101+Arkopravo1995@users.noreply.github.com>
2022-07-22 11:33:31 +0530

    Update Arrayex2.java

--- Arrayex2.java
+++ Arrayex2.java
@@ -6,12 +6,12 @@ public class Arrayex2 {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("enter the array size: ");
 		int n = sc.nextInt(); //enter the array size
-		int arr[]=  new int[n];
-		for(int i = 0 ; i < n ; i++) {
+		int arr[]=  new int[n]; 
+		for(int i = 0 ; i < n ; i++) { //initilizing array
 			arr[i] = sc.nextInt();
 			
 		}
-		for(int i = 0 ; i<n ;i++) {
+		for(int i = 0 ; i<n ;i++) {  //display array
 			System.out.print(arr[i]);
 		}
 		
